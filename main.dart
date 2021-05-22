98b5b3306f695f721b681e87e5c2db583f478d37
Sirirak Witoon <nutsrk@Siriraks-MacBook-Pro.local>
2021-05-22 11:45:01 +0700

    create login page

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main(){
+  String name = "Nutsrk";
+  Map<String, dynamic> address ={
+    "myid": "3144",
+    "myphone": 2345,
+  };
+  print(name);
+  print(address["myid"]);
+
+  int age;
+  age = 22;
+  print(age); 
+}
