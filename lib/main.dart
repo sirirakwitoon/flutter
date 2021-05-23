5a417987d04df00daa7a0679cd7bd870bdde916e
Aphirat Nimanussonkul <Tata@odds.team>
2021-05-23 09:46:32 +0700

    create folder login and move login screen on it

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:myapp/screens/login.dart';
+import 'package:myapp/screens/login/login.dart';
 
 void main() {
   runApp(MyApp());
